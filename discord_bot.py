5b8752bf92adf73806c998849312b310db2c91fb
Hoshimi <Petrichor65535@gmail.com>
2019-07-06 23:41:37 +0900

    v4.1.7.2

--- discord_bot.py
+++ discord_bot.py
@@ -1,4 +1,4 @@
-#=====VERSION:4.1.7=====
+#=====VERSION:4.1.7.2=====
 
 import discord
 import random
@@ -15,14 +15,14 @@ import textfunc as func
 import subprocess
 import datetime
 
-VERSION = "4.1.7"
+VERSION = "4.1.7.2"
 client = discord.Client()
 t = datetime.datetime.now
 rep_list = ("ごきげんよう","どちらさまでしょうか？","しらん","帰れ")
 vc_list = ["neverdie", "sumanko", "airhorn", "goldrush1", "goldrush2",
             "scream","yarimasunee","kowareruwa","yurusite","soudayo",
             "haha","USSR","roboto","mankoja","jamayaro","oi","ike","dase",
-            "keshigomu","marmelo","saikyou","TF","doko","hujino","suiso"]
+            "keshigomu","marmelo","saikyou","TF","doko","hujino","h2"]
 func_list = {
     "join" : func.join,
     "leave" : func.leave,
