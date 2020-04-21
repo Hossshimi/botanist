478ef61a691c7673f9db194d528af894caa59710
Hoshimi <Petrichor65535@gmail.com>
2020-04-21 20:16:55 +0900

    for YM

--- discord_bot.py
+++ discord_bot.py
@@ -1,4 +1,4 @@
-VERSION = "5.0.0"
+VERSION = "5.0.2"
 
 import discord
 import random
@@ -60,7 +60,7 @@ COR_LIST = ["join","leave","honda","nick","MO"]
     print(c.name)"""
 vc_id = None
 player = {}
-airhorn_flag = False
+YM_flag = True
 vc_lock = False
 pflag = False
 LOG_CHANNEL = None
@@ -110,25 +110,29 @@ async def on_ready(): #-------起動時処理-------------
     except: pass
     finally: pass
 
-"""@client.event
-async def on_voice_state_update(before, after): #VC参加時にairhorn
-    global airhorn_flag,pflag
-    if airhorn_flag and (not pflag) :
-        if before.voice_channel is None:
-            global vc_id
-            channel = client.get_channel(vc_id)
-            global player
+@client.event
+async def on_voice_state_update(member,before, after): #VC参加時にairhorn
+    global YM_flag,pflag
+    if YM_flag and (member.id == 482875226502332416) and (not pflag) :
+        if before.channel == None:
+            #global vc_id
+            #channel = after.channel
+            vc_id = after.channel.id
+            #global player
             try:
-                player.stop()
+                player[vc_id].stop()
             except:
                 pass
             finally:
-                if client.is_voice_connected(channel.server):
-                    voice = client.voice_client_in(channel.server)
-                else:
-                    voice = await client.join_voice_channel(channel)
-                player = voice.create_ffmpeg_player(r'D:\Backup\work\discord_bot\sounds\airhorn.mp3')
-                player.start()"""
+                #try:
+                #    if func.vc[vc_id]:
+                #        voice = client.voice_client_in(channel.server)
+                #except:
+                #    voice = await client.join_voice_channel(channel)
+                #finally:
+                    sleep(1)
+                    func.vc[vc_id].play(discord.FFmpegPCMAudio("sounds\\i_love.mp3"))
+                    #player.start()
 
 
 @client.event
@@ -204,7 +208,7 @@ async def on_message(message):
             reply += "```"
             await message.channel.send(reply)
 
-        global player,airhorn_flag,vc_lock,FONTPATH,FUNC_LIST
+        global player,vc_lock,FONTPATH,FUNC_LIST
         #vc_id = "317228479416500227" #chikwa
         #old_id = ""
         #channel = client.get_channel(vc_id)
